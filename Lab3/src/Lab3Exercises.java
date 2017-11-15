db9e35b0f9bb30ba4c2a51577118ca410c1939c0
4015e3 <4015e3@zapote.ece.uprm.edu>
2017-11-15 10:49:12 -0400

    Changed the JFrame title to my name, and the JFrame size to 300 x 300. Also, adjusted the location so that the window shows up somewhat approximately in the center of the display when the program is run.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -5,7 +5,7 @@ public class Lab3Exercises {
 		JFrame myFrame = new JFrame("Jose Bermudez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(800, 400);
-		myFrame.setSize(200, 200);
+		myFrame.setSize(300, 300);
 		myFrame.setVisible(true);
 	}
 }
